7f9e6287423e239c0f715b8a4f7c99d399c6bd67
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2019-07-22 21:20:40 +0300

    Updates

---
+++ precache-manifest.0e62b61d88fa584a9aa3d8510212d111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9becd3644a523252f77aff713d7527b1",
+    "url": "/OffScreenPanel/index.html"
+  },
+  {
+    "revision": "23a5655551abf2c538fc",
+    "url": "/OffScreenPanel/static/css/main.b6c2a63c.chunk.css"
+  },
+  {
+    "revision": "e5627303157ee693b770",
+    "url": "/OffScreenPanel/static/js/2.811c7575.chunk.js"
+  },
+  {
+    "revision": "23a5655551abf2c538fc",
+    "url": "/OffScreenPanel/static/js/main.1eb70afa.chunk.js"
+  },
+  {
+    "revision": "ff5271656e04af537894",
+    "url": "/OffScreenPanel/static/js/runtime~main.871a5b13.js"
+  },
+  {
+    "revision": "49b6bbdd8f3c036f7ac303cc7e43202f",
+    "url": "/OffScreenPanel/static/media/logo.49b6bbdd.svg"
+  }
+]);
